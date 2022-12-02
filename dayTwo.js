04895fbbaf5baffeeeba9f721f60b3eadd20f466
Doug Whitley <93144479+DougWhitley@users.noreply.github.com>
2022-12-02 19:43:29 +0000

    cleanup

--- dayTwo.js
+++ dayTwo.js
@@ -1,8 +1,39 @@
 import { input } from "./Inputs/Day2Input.js";
+
 export function dayTwo(){
     const parsedInput = input.split('\n');
     console.log(partOne(parsedInput));
     console.log(partTwo(parsedInput));
+}
+
+function partOne(input){
+    let score = 0;
+    input.forEach(element => {
+        const e = element.split(' ');
+        score += throwLookup[lookup[e[1]]].score + winScore(lookup[e[1]], lookup[e[0]]);
+    });
+    return score;
+}
+
+function winScore(playerThrow, opponentThrow){
+    if(playerThrow === opponentThrow) return draw;
+    if(throwLookup[playerThrow].winsAgainst === opponentThrow) return win;
+    return lose;
+}
+
+function partTwo(input){
+    let score = 0;
+    input.forEach(element => {
+        let e = element.split(' ');
+        score += outcomeScore(lookupPartTwo[e[1]], lookup[e[0]]);
+    });
+    return score;
+}
+
+function outcomeScore(outcome, opponentThrow){
+    if(outcome === 'Draw') return throwLookup[opponentThrow].score + draw;
+    if(outcome === 'Win') return throwLookup[throwLookup[opponentThrow].losesAgainst].score + win;
+    return throwLookup[throwLookup[opponentThrow].winsAgainst].score;
 }
 
 const throwLookup = {
@@ -32,47 +63,12 @@ const lookup = {
     'Z' : 'Scissors'
 }
 
-function partOne(input){
-    let score = 0;
-    input.forEach(element => {
-        const e = element.split(' ');
-        score += throwLookup[lookup[e[1]]].score + winScore(lookup[e[1]], lookup[e[0]]);
-    });
-    return score;
-}
-
-function winScore(playerThrow, opponentThrow){
-    if(playerThrow === opponentThrow){
-        return 3;
-    }
-    if(throwLookup[playerThrow].winsAgainst === opponentThrow){
-        return 6;
-    }
-    return 0;
-}
-
 const lookupPartTwo = {
     'X' : 'Lose',
     'Y' : 'Draw',
     'Z' : 'Win'
 }
 
-function partTwo(input){
-    let score = 0;
-    input.forEach(element => {
-        let e = element.split(' ');
-        score += outcomeScore(lookupPartTwo[e[1]], lookup[e[0]]);
-    });
-    return score;
-}
-
-function outcomeScore(outcome, opponentThrow){
-    if(outcome === 'Draw'){
-        return throwLookup[opponentThrow].score + 3;
-    }
-    if(outcome === 'Win')
-    {
-        return throwLookup[throwLookup[opponentThrow].losesAgainst].score + 6;
-    }
-    return throwLookup[throwLookup[opponentThrow].winsAgainst].score;
-}
+const win = 6;
+const draw = 3;
+const lose = 0;
